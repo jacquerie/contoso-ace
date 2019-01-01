c381df137627def0cb1d4bd389e36258b69eecff
Jacopo Notarstefano <jacopo.notarstefano@gmail.com>
2019-01-01 20:09:35 +0100

    client: switch to react

---
+++ app/client/build/precache-manifest.abb982b19d1ff945a11897e387543942.js
@@ -0,0 +1,18 @@
+self.__precacheManifest = [
+  {
+    "revision": "1c98815b532f7fdb4758",
+    "url": "/static/js/main.1c98815b.chunk.js"
+  },
+  {
+    "revision": "4a34e45d34cafb7623bd",
+    "url": "/static/js/1.4a34e45d.chunk.js"
+  },
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "706d75113981904979cb4e43baba3d8d",
+    "url": "/index.html"
+  }
+];
