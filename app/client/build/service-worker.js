7898420d6aec551c65ec21cae76b2ff532435c8e
Jacopo Notarstefano <jacopo.notarstefano@gmail.com>
2019-01-02 04:11:00 +0100

    client: add login component

--- app/client/build/service-worker.js
+++ app/client/build/service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.6970278a768a77c8e2febb2c681b5af4.js"
+  "/precache-manifest.1d62e4b8f90c1051e616deab50a47617.js"
 );
 
 workbox.clientsClaim();
