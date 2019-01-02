467c2e46665f290a69e802a1b154fa5cd9eaab32
Jacopo Notarstefano <jacopo.notarstefano@gmail.com>
2019-01-02 04:11:00 +0100

    client: add login component

--- app/client/src/Login.js
+++ app/client/src/Login.js
@@ -1,10 +1,47 @@
 import React from 'react';
+import {
+  Button,
+  Card,
+  CardBody,
+  Col,
+  Container,
+  Form,
+  FormGroup,
+  Input,
+  Label,
+  Row,
+} from 'reactstrap';
+
+import './Login.css';
 
 class Login extends React.Component {
   render() {
     return (
       <div className="Login">
-        <h2>Login</h2>
+        <Container>
+          <Row>
+            <Col xs={{ offset: 3, size: 6 }}>
+              <Card>
+              <CardBody>
+              <Form>
+                <FormGroup>
+                  <Label for="Email">Email</Label>
+                  <Input
+                    type="email" name="Email" id="Email"
+                    placeholder="barbara@contoso.com"
+                  />
+                </FormGroup>
+                <FormGroup>
+                  <Label for="Password">Password</Label>
+                  <Input type="password" name="Password" id="Password" />
+                </FormGroup>
+                <Button color="primary">Sign In</Button>
+              </Form>
+              </CardBody>
+              </Card>
+            </Col>
+          </Row>
+        </Container>
       </div>
     );
   }
