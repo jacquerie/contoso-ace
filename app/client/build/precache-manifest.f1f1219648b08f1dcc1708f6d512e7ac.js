4394e08506a6cdc5ba23d0879cb56da567446b9d
Jacopo Notarstefano <jacopo.notarstefano@gmail.com>
2019-01-02 04:32:33 +0100

    client: add dashboard component

---
+++ app/client/build/precache-manifest.f1f1219648b08f1dcc1708f6d512e7ac.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "3f686a5228d326e0819f",
+    "url": "/static/css/main.a5daaeaf.chunk.css"
+  },
+  {
+    "revision": "3f686a5228d326e0819f",
+    "url": "/static/js/main.3f686a52.chunk.js"
+  },
+  {
+    "revision": "0fcb115c3d94fdf60c27",
+    "url": "/static/css/1.a0abb61c.chunk.css"
+  },
+  {
+    "revision": "0fcb115c3d94fdf60c27",
+    "url": "/static/js/1.0fcb115c.chunk.js"
+  },
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "34488ec368e86b97e7e18fe5a653ca4e",
+    "url": "/index.html"
+  }
+];
--- app/client/src/Dashboard.js
+++ app/client/src/Dashboard.js
@@ -1,10 +1,37 @@
 import React from 'react';
+import {
+  Button,
+  Card,
+  CardBody,
+  CardText,
+  CardTitle,
+  Col,
+  Container,
+  Row,
+} from 'reactstrap';
+
+import './Dashboard.css'
 
 class Dashboard extends React.Component {
   render() {
     return (
       <div className="Dashboard">
-        <h2>Dashboard</h2>
+        <Container>
+          <Row>
+            <Col xs="4">
+              <Card>
+                <CardBody>
+                  <CardTitle tag="h2">Jane Doe</CardTitle>
+                  <CardText>
+                    Hi, I've got a trip to Paris next Monday
+                    and I'd like to rent a car there.
+                  </CardText>
+                  <Button color="success">Accept</Button>
+                </CardBody>
+              </Card>
+            </Col>
+          </Row>
+        </Container>
       </div>
     );
   }
