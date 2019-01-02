ec23b848a9de2e75dce35b10ea8c3d488772a2d7
Jacopo Notarstefano <jacopo.notarstefano@gmail.com>
2019-01-02 03:18:15 +0100

    client: add header component

--- app/client/src/App.js
+++ app/client/src/App.js
@@ -2,13 +2,14 @@ import React from 'react';
 import { BrowserRouter as Router, Route } from 'react-router-dom';
 
 import Chats from './Chats';
+import Header from './Header';
 import Login from './Login';
 
 class App extends React.Component {
   render() {
     return (
       <div className="App">
-        <h1>Contoso ACE</h1>
+        <Header />
         <Router>
           <div className="Router">
             <Route path="/" exact component={Login} />
